624de1f50391a516dd7b9f3536754c64bee9788c
Wizard Bot 86 <61592605+Wizard-Bot-86@users.noreply.github.com>
2021-07-17 18:01:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Camera from './Screens/Camera';
+
+export default function App() {
+  return <Camera />;
+}
